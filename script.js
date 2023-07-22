6e9f55d69d5cd76d2f754593a5e215a2cb7b6977
ramazangalip <ramazansaidgalip@gmail.com>
2023-07-22 13:40:11 +0300

    22.07.2023

---
+++ script.js
@@ -0,0 +1,10 @@
+function differenceMultipleAdded(sayi1,sayi2){
+    return (sayi1*sayi2) - (sayi1+sayi2)
+}
+console.log(differenceMultipleAdded(7,2))
+
+function findChildren(sayi){
+    return (3*3) + (1*1)
+
+}
+console.log(findChildren(12))
